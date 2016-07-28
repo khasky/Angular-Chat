767e60fc0eb797ab3723b3801147df100b7a52d9
Khasky <ionkhasky@gmail.com>
2016-07-28 13:28:45 +0300

    Grunt project optimization

--- gruntfile.js
+++ gruntfile.js
@@ -2,60 +2,48 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		concat: {
-			dist: {
-				src: [
-					'src/js/angular.min.js',
-					'src/js/angular-animate.min.js',
-					'src/js/angular-sanitize.min.js',
-				],
-				dest: 'build/js/angular.custom.js'
-			}
-		},
 		uglify: {
-			dist: {
+			main: {
 				files: [
-					{ src: 'build/js/angular.custom.js', dest: 'build/js/angular.custom.min.js' },
 					{ src: 'src/ngchat/ng-chat.js', dest: 'build/js/ng-chat.min.js' }
 				]
 			}
 		},
-        less: {
-            development: {
-                options: {
-                    paths: ['src/less/']
-                },
-                files: {
-                    'build/css/ng-chat.css': 'src/less/ng-chat.less'
-                }
-            }
-        },
-		cssmin: {
-			options: {
-				shorthandCompacting: false,
-				roundingPrecision: -1
+		clean: {
+			pre: {
+				src: ['dist']
 			},
-			target: {
-				files: {
-					'build/css/ng-chat.min.css': 'build/css/ng-chat.css' //['src/css/grid100.min.css', 'build/css/ng-chat.css']
-				}
+			post: {
+				src: ['build']
 			}
 		},
+        less: {
+            main: {
+                options: {
+					compress: true,
+					paths: ['src/less/']
+				},
+                files: { 'dist/ngchat/ng-chat.min.css': 'src/less/ng-chat.less' }
+            }
+        },
 		copy: {
 			main: {
 				files: [
 					// libs
-					{ src: 'src/js/jquery-2.2.3.min.js', dest: 'dist/js/jquery-2.2.3.min.js' },
-					{ src: 'src/js/lodash.min.js', dest: 'dist/js/lodash.min.js' },
-					{ src: 'build/js/angular.custom.min.js', dest: 'dist/js/angular.custom.min.js'},
+					{ src: 'src/libs/jquery.min.js', dest: 'dist/libs/jquery.min.js' },
+					{ src: 'src/libs/lodash.min.js', dest: 'dist/libs/lodash.min.js' },
+					{ src: 'src/libs/angular.min.js', dest: 'dist/libs/angular.min.js'},
+					{ src: 'src/libs/angular-sanitize.min.js', dest: 'dist/libs/angular-sanitize.min.js'},
+					// css
 					{ src: 'src/css/grid100.min.css', dest: 'dist/css/grid100.min.css' },
 					// ngchat
+					// - backend
 					{ src: 'src/ngchat/config.php', dest: 'dist/ngchat/config.php' },
 					{ src: 'src/ngchat/admin.php', dest: 'dist/ngchat/admin.php' },
 					{ src: 'src/ngchat/read.php', dest: 'dist/ngchat/read.php' },
 					{ src: 'src/ngchat/send.php', dest: 'dist/ngchat/send.php' },
 					{ src: 'src/ngchat/status.php', dest: 'dist/ngchat/status.php' },
-					{ src: 'build/css/ng-chat.min.css', dest: 'dist/ngchat/ng-chat.min.css' },
+					// - frontend
 					{ src: 'src/ngchat/ng-chat.html', dest: 'dist/ngchat/ng-chat.html' },
 					{ src: 'build/js/ng-chat.min.js', dest: 'dist/ngchat/ng-chat.min.js' },
 					{ src: 'src/smilies/*', dest: 'dist/ngchat/smilies/', expand: true, flatten: true, filter: 'isFile' },
@@ -64,19 +52,13 @@ module.exports = function(grunt) {
 					{ src: 'src/app.js', dest: 'dist/app.js' }
 				]
 			}
-		},
-		watch: {
-            files: 'src/less/*.less',
-            tasks: ['less', 'cssmin']
-        }
+		}
 	});
-
-	grunt.loadNpmTasks('grunt-contrib-concat');
+	
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-copy');
-
-	grunt.registerTask('default', ['concat', 'uglify', 'less', 'cssmin', 'copy']); //, 'watch'
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	
+	grunt.registerTask('default', ['clean:pre', 'uglify', 'less', 'copy', 'clean:post']);
 };
